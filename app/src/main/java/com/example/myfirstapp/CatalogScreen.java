971c5f3af50ad2f3d0f68c4220f6f03dff43e0bd
Lindsey Wingate <lwingate@waltersandwolf.com>
2020-06-24 16:53:09 -0700

    about to try getting a cardview

--- app/src/main/java/com/example/myfirstapp/CatalogScreen.java
+++ app/src/main/java/com/example/myfirstapp/CatalogScreen.java
@@ -3,14 +3,24 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class CatalogScreen extends AppCompatActivity {
 
@@ -21,21 +31,28 @@ public class CatalogScreen extends AppCompatActivity {
         final TextView textView = (TextView) findViewById(R.id.text);
 
         /*REQUEST *************************************************************/
+        // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=LindseyW-searchhw-PRD-02eb6be68-006d798c&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD";
+        String url ="https://www.google.com";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                textView.setText("Response is: " + response.substring(0, 500));
-            }
-        }, new Response.ErrorListener() {
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("It didn't work");
+                textView.setText("That didn't work!");
             }
         });
+
+// Add the request to the RequestQueue.
         queue.add(stringRequest);
 
     }
+
 }
