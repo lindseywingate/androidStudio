bdb83b8d5b4c108720191b2f68f191e4d1210a9f
Lindsey Wingate <lwingate@waltersandwolf.com>
2020-06-24 22:00:13 -0700

    uninstalled and reinstalled app and it worked!

--- app/src/main/java/com/example/myfirstapp/CatalogScreen.java
+++ app/src/main/java/com/example/myfirstapp/CatalogScreen.java
@@ -1,16 +1,23 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
@@ -24,35 +31,39 @@ import java.io.UnsupportedEncodingException;
 
 public class CatalogScreen extends AppCompatActivity {
 
+    private RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog_screen);
         final TextView textView = (TextView) findViewById(R.id.text);
-
+        String url = "https://jsonplaceholder.typicode.com/users";
         /*REQUEST *************************************************************/
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://www.google.com";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
+                    public void onResponse(JSONArray response) {
+                        Log.d("test", "response!");
+                        textView.setText("trimmed response" + response.toString());
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("test", error.getMessage());
+                        Toast.makeText(getApplicationContext(), "Nothing found", Toast.LENGTH_SHORT);
+                }
         });
 
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        requestQueue.add(jsonArrayRequest);
+        //RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //recyclerView.setHasFixedSize(true);
+        //LinearLayoutManager llm = new LinearLayoutManager(this);
+        //recyclerView.setLayoutManager(llm);
 
+
+        //specify an adapter
     }
 
 }
